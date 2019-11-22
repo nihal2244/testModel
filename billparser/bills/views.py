633be64ba1dd78c9626c294600d31d61a7b09ea8
nihal2244 <nihal2244@yahoo.com>
2019-11-22 20:42:08 +0530

    api test

--- billparser/bills/views.py
+++ billparser/bills/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from rest_framework import viewsets,status
+from rest_framework import viewsets,status,filters
 from rest_framework.response import Response
 from .models import User,Bills
 from .serializers import billSerializer
@@ -8,16 +8,21 @@ from .serializers import billSerializer
 
 class EndUser(viewsets.ViewSet):
     def list(self, request):
-        queryset = Bills.objects.all()
-        serializer = billSerializer(queryset, many=True)
-        return Response(serializer.data)
+        user = request.POST["user_id"]
+        if user:
+            bill_list=Bills.objects.filter(user_id=user)
+            serializer = billSerializer(bill_list,many=True)
+            return Response(serializer.data)
+        else:
+            return Response({"error":"user id required"})
 
     def create(self, request):
-         serializer.save(owner=self.request.user,
-                       datafile=self.request.data.get('datafile'))
-                       
-#     def retrieve(self, request, pk=None):
-#         pass
+        file_serializer = billSerializer(data=request.data)
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
    
 class InternalUser(viewsets.ViewSet):
@@ -42,17 +47,22 @@ class InternalUser(viewsets.ViewSet):
 
     def update(self, request, pk=None):
         queryset = Bills.objects.all()
-        print(queryset)
-        serializer = billSerializer(instance=queryset,data=request.data)
+        bills = get_object_or_404(queryset, pk=pk)
+        serializer = billSerializer(instance=bills,data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
     def partial_update(self, request, pk=None):
-        serializer = billSerializer(request.user, data=request.data, partial=True)
-        serializer.save()
+        queryset = Bills.objects.all()
+        bills = get_object_or_404(queryset, pk=pk)
+        serializer = billSerializer(instance=bills,data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
 
-    # def destroy(self, request, pk=None):
-    #     pass
+    def destroy(self, request, pk=None):
+        queryset = Bills.objects.all()
+        bills = get_object_or_404(queryset, pk=pk)
+        bills.delete()
+        return Response("deleted")
